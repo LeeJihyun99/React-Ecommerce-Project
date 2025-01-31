a2390aa29a7bb3f01387c14d2ad8643972a723a0
ljihyun99 <Ji.Lee@campus.lmu.de>
2025-01-31 15:48:05 +0100

    added scss for categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-import "./App.css";
+import "./categories.styles.scss";
 
 const App = () => {
-  return <>hello</>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Womens",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title }) => {
+        return (
+          <div key={id} className="category-container">
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Show now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
